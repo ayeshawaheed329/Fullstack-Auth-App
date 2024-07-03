90a7ce3b57bff3fa171030fd81d2c6b918e2d0e3
Ayesha Waheed <ayeshawaheed@folio3.com>
2024-07-04 00:41:17 +0500

    Backend main file configurations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,52 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+// module
+import helmet from 'helmet';
+import express from 'express';
+
+// exception filter
+import { AllExceptionsFilter } from './all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Create NestJS app instance
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['error', 'warn'], // log warnings and error
+  });
+
+  // Enable trust proxy to handle proxy headers like X-Forwarded-For
+  app.enable('trust proxy');
+
+  // middleware for setting HTTP headers securely
+  app.use(helmet());
+
+  // enable configurations
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+  };
+  app.enableCors(options);
+
+  // Use express.urlencoded middleware for parsing form data
+  app.use(
+    express.urlencoded({
+      limit: '50mb',
+      extended: true,
+      parameterLimit: 50000, // dummy value
+    }),
+  );
+
+  // Get the HttpAdapterHost instance
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  // Register the AllExceptionsFilter globally
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
+  // Start the application and listen on given port
   await app.listen(3001);
 }
+
 bootstrap();
